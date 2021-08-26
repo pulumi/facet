951292b6ea860e75fb037dbac32e56130a8f9970
Christian Nunciato <c@nunciato.org>
2021-08-26 12:28:17 -0700

    Storybook fixes

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,60 @@
-import { initDesignSystem, counter, accordion, accordionItem, disclosure } from "../src";
+import { initDesignSystem, counter, accordion, accordionItem, disclosure, tab, tabs, tabPanel, combobox, button, card, checkbox, option } from "../src";
+import { accordionStyles, accordionItemStyles, cardStyles, comboboxStyles, tabStyles, tabsStyles, tabPanelStyles, optionStyles, checkboxStyles } from "@microsoft/fast-components";
+import { wwwTabStyles, wwwTabsStyles, wwwTabPanelStyles } from "../src/design/theme/web";
+import { css } from "@microsoft/fast-element";
 
 initDesignSystem({
     prefix: "pulumi",
     theme: "light",
     components: [
-        accordion(),
-        accordionItem(),
+        accordion({
+            styles: (ctx: any, def: any) => css`
+                ${accordionStyles(ctx, def)}
+            `,
+        }),
+        accordionItem({
+            styles: (ctx: any, def: any) => css`
+                ${accordionItemStyles(ctx, def)}
+            `,
+        }),
+        button(),
+        card({
+            styles: (ctx: any, def: any) => css`
+                ${cardStyles(ctx, def)}
+            `,
+        }),
+        checkbox({
+            styles: (ctx: any, def: any) => css`
+                ${checkboxStyles(ctx, def)}
+            `,
+        }),
+        combobox({
+            styles: (ctx: any, def: any) => css`
+                ${comboboxStyles(ctx, def)}
+            `,
+        }),
         counter(),
         disclosure(),
+        option({
+            styles: (ctx: any, def: any) => css`
+                ${optionStyles(ctx, def)}
+            `,
+        }),
+        tab({
+            styles: (ctx: any, def: any) => css`
+                ${tabStyles(ctx, def)}
+            `,
+        }),
+        tabs({
+            styles: (ctx: any, def: any) => css`
+                ${tabsStyles(ctx, def)}
+            `,
+        }),
+        tabPanel({
+            styles: (ctx: any, def: any) => css`
+                ${tabPanelStyles(ctx, def)}
+            `,
+        }),
     ],
 });
 
--- src/stories/Tabs.stories.ts
+++ src/stories/Tabs.stories.ts
@@ -21,13 +21,19 @@ export interface TabsProps {
 
 const Template: Story<TabsProps> = ({ orientation }) => {
     return `
-        <pulumi-tabs orientation="${orientation}">
+        <pulumi-tabs orientation="${orientation || "horizontal"}">
             <pulumi-tab slot="tab" id="tab-1">Tab one</pulumi-tab>
             <pulumi-tab slot="tab" id="tab-2">Tab two</pulumi-tab>
             <pulumi-tab slot="tab" id="tab-3">Tab three</pulumi-tab>
-            <pulumi-tab-panel slot="tabpanel">Tab panel 1</pulumi-tab-panel>
-            <pulumi-tab-panel slot="tabpanel">Tab panel 2</pulumi-tab-panel>
-            <pulumi-tab-panel slot="tabpanel">Tab panel 3</pulumi-tab-panel>
+            <pulumi-tab-panel slot="tabpanel">
+                <p>Tab panel 1</p>
+            </pulumi-tab-panel>
+            <pulumi-tab-panel slot="tabpanel">
+                <p>Tab panel 2</p>
+            </pulumi-tab-panel>
+            <pulumi-tab-panel slot="tabpanel">
+                <p>Tab panel 3</p>
+            </pulumi-tab-panel>
         </pulumi-accordion>
     `;
 };
