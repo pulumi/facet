a495e59a314f8e2ccee778bd331e44f08000de6b
Christian Nunciato <c@nunciato.org>
2021-09-07 22:04:05 -0700

    Add infra program

---
+++ infra/index.ts
@@ -0,0 +1,27 @@
+import * as pulumi from "@pulumi/pulumi";
+import { Website } from "@cnunciato/pulumi-jamstack-aws";
+
+const config = new pulumi.Config();
+const domainHost = config.require("domainHost");
+const domainName = config.require("domainName");
+const websitePath = config.require("websitePath");
+
+const site = new Website("website", {
+    site: {
+        path: websitePath,
+    },
+    domain: {
+        host: domainHost,
+        name: domainName,
+    },
+});
+
+export const {
+    bucketName,
+    bucketWebsiteURL,
+    cdnDomainName,
+    cdnURL,
+    apiGatewayURL,
+    websiteURL,
+    websiteLogsBucketName,
+} = site;
