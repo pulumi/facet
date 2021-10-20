0c213e1eb701215bbcca7a82c9c8303b4ed1aa3a
Kimberley Mackenzie <kimberley@pulumi.com>
2021-10-20 11:40:42 -0700

    Remove storybook examples and assets.

--- packages/facet/src/stories/examples/Header.d.ts
+++
@@ -1,9 +0,0 @@
-import './header.css';
-export interface HeaderProps {
-    user?: {};
-    onLogin: () => void;
-    onLogout: () => void;
-    onCreateAccount: () => void;
-}
-export declare const createHeader: ({ user, onLogout, onLogin, onCreateAccount }: HeaderProps) => HTMLElement;
-//# sourceMappingURL=Header.d.ts.map
